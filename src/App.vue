6bf80f279db5475d313afb9696d9ea047408e40b
EmilDanielSamfirescu <120398411+EmilDanielSamfirescu@users.noreply.github.com>
2023-07-17 16:26:44 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script >
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+  export default {
+    components: {
+      HeaderComponent,
+      MainComponent,
+      FooterComponent
+  },
+  data (){
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<template>
+<HeaderComponent />
+<MainComponent />
+<FooterComponent />
+</template>
+
+<style lang="scss">
+@use "./assets/scss/main.scss";
+
+</style>
