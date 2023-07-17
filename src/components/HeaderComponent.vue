fe74a50b544e48ebd2efe817d9bc6dbb92f22d0b
EmilDanielSamfirescu <emil.danielsam@gmail.com>
2023-07-17 17:07:14 +0200

    headercomponent Fatto

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -13,11 +13,39 @@ methods: {
 </script>
 
 <template>
-    <header>
-        HEADER
+    <header class="d-flex">
+
+        <div class="img-container align-middle">
+            <img src="../assets/img/pngegg.png" alt="">
+        </div>
+
+        <h1>
+            Yu-Gi-Oh Api
+        </h1>
+
     </header>
 </template>
 
 <style lang="scss" scoped>
+
+header {
+    padding-left: 30px;
+    height: 100px;
+
+    h1 {
+        line-height: 100px;
+        padding-left: 30px;
+    }
+    .img-container {
+        width: 100px;
+        display: block;
+    
+        img {
+            width: 100%;
+            display: block;
+        }
+    }
+
+}
 
 </style>
